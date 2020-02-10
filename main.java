2e3a1b40d51044ddf2c1f7e4a7ac12a55ec69781
MichOchieng <MichOchieng@gmail.com>
2020-02-09 22:23:29 -0800

    First commit

---
+++ main.java
@@ -0,0 +1,17 @@
+
+public class main{    
+    public static void main(String[] args){        
+        cuckoo c = new cuckoo(11);
+        c.add(3);
+        c.add(5);
+        c.add(16);
+        c.add(27);
+        c.add(38); 
+        c.add(33); 
+        c.add(49);   
+        c.add(44); 
+        c.add(133); 
+        c.add(121);        
+    }
+
+}
