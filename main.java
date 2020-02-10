6c5c3a84d060479d56b95fb30ffc95fdda35d0dc
MichOchieng <MichOchieng@gmail.com>
2020-02-09 23:11:49 -0800

    Added more tests

--- main.java
+++ main.java
@@ -1,7 +1,7 @@
-
 public class main{    
     public static void main(String[] args){        
         cuckoo c = new cuckoo(11);
+        // Test for adding values
         c.add(3);
         c.add(5);
         c.add(16);
@@ -11,7 +11,16 @@ public class main{
         c.add(49);   
         c.add(44); 
         c.add(133); 
-        c.add(121);        
+        // Rehash test
+        c.add(121);
+        // Deletion test  
+        c.evict(121); 
+        c.evict(3000);
+        // Lookup test
+        c.lookup(44);
+        c.lookup(3000);
+        
+        
     }
 
 }
